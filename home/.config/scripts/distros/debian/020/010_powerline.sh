39fd19412bd3d8b67f44ae57f0c47d32295f46bb
shawngmc <shawngmc@gmail.com>
2023-07-29 01:32:16 -0400

    update powerline in debian

--- home/.config/scripts/distros/debian/020/010_powerline.sh
+++ home/.config/scripts/distros/debian/020/010_powerline.sh
@@ -5,10 +5,14 @@ sudo apt install powerline
 
 echo "Configuring .bashrc..."
 cat <<-"EOF1" > $HOME/.bashrc.append
-powerline-daemon -q
+
+STATUS="$(systemctl is-active powerline-daemon.service --user)"
+if [ "${STATUS}" != "active" ]; then
+    systemctl start powerline-daemon.service --user
+fi
 POWERLINE_BASH_CONTINUATION=1
 POWERLINE_BASH_SELECT=1
-. {repository_root}/powerline/bindings/bash/powerline.sh
+. /usr/share/powerline/bindings/bash/powerline.sh
 EOF1
 if ! grep -F -q -f $HOME/.bashrc{.append,}; then
     cat $HOME/.bashrc.append >> $HOME/.bashrc
