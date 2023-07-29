2b74f70a2fc5c3da55ca79099f463aa6f0da8eb3
shawngmc <shawngmc@gmail.com>
2023-07-29 01:40:04 -0400

    powerline fixes

--- home/.config/scripts/distros/debian/020/010_powerline.sh
+++ home/.config/scripts/distros/debian/020/010_powerline.sh
@@ -4,8 +4,7 @@ echo "Installing powerline..."
 sudo apt install powerline
 
 echo "Configuring .bashrc..."
-cat <<-"EOF1" > $HOME/.bashrc.append
-
+cat >> $HOME/.bashrc <<'EOF'
 STATUS="$(systemctl is-active powerline-daemon.service --user)"
 if [ "${STATUS}" != "active" ]; then
     systemctl start powerline-daemon.service --user
@@ -13,7 +12,4 @@ fi
 POWERLINE_BASH_CONTINUATION=1
 POWERLINE_BASH_SELECT=1
 . /usr/share/powerline/bindings/bash/powerline.sh
-EOF1
-if ! grep -F -q -f $HOME/.bashrc $HOME/.bashrc.append; then
-    cat $HOME/.bashrc.append >> $HOME/.bashrc
-fi
+EOF
