2be4682decaf41e38e17b962ebe0d4e258962c31
shawngmc <shawngmc@gmail.com>
2023-08-01 02:34:48 -0400

    add gnome-terminal customization script

---
+++ home/.config/scripts/apps/gnome-terminal.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Default:"
+echo "- New Tab: Ctrl-Shift-T"
+
+echo "Next Tab: Ctrl-Tab"
+gsettings set org.gnome.Terminal.Legacy.Keybindings:/org/gnome/terminal/legacy/keybindings/ next-tab '<Primary>Tab'
+echo "Previous Tab: Ctrl-Shift-Tab"
+gsettings set org.gnome.Terminal.Legacy.Keybindings:/org/gnome/terminal/legacy/keybindings/ prev-tab '<Primary><Shift>Tab'
