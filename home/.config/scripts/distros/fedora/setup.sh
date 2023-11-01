4c0bfc4fe5f329009b034cf6bad307e598dc889c
shawngmc <7342147+shawngmc@users.noreply.github.com>
2023-11-01 15:25:52 -0400

    Update setup.sh
    Add glances, use array for package list

--- home/.config/scripts/distros/fedora/setup.sh
+++ home/.config/scripts/distros/fedora/setup.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-# Neofetch
-sudo yum install neofetch
+PACKAGES=()
 
-# CLI Clipboard
-sudo yum install wl-clipboard
+# Neofetch - system info at-a-glance in CLI for .bashrc
+PACKAGES+=("neofetch")
+
+# wl-clipboard - CLI Clipboard tools for wayland
+PACKAGES+=("wl-clipboard")
+
+# Glances - top alternative
+PACKAGES+=("glances")
+
+sudo yum install -y ${PACKAGES[@]}
