db59d10a528c54ded27d293038855fc880cbac41
Shawn <shawngmc@gmail.com>
2024-02-19 04:38:00 -0500

    neovim install cleanup

--- home/.config/scripts/install/cli/vim.sh
+++ home/.config/scripts/install/cli/vim.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 source /etc/os-release
-PACKAGES=()
 
 # Have to save pull from GH to protect newlines
-curl -s "https://api.github.com/repos/neovim/neovim/releases/latest" -o ./jq_latest.json 
-NEOVIM_TAG=$(cat ./neovim_latest.json | grep tag-name)
+curl -s "https://api.github.com/repos/neovim/neovim/releases/latest" -o ./neovim_latest.json 
+NEOVIM_TAG=$(cat ./neovim_latest.json | grep tag_name)
 NEOVIM_TAG=${NEOVIM_TAG:15:-2}
-echo "Pulling JQ tag: ${NEOVIM_TAG}"
+echo "Pulling Neovim tag: ${NEOVIM_TAG}"
 
-NEOVIM_LATEST_URL=$(cat ./neovim_latest.json | grep browser_download_url | grep nvim-linux64.tar.gz)
-NEOVIM_LATEST_URL=${NEOVIM_LATEST_URL:31:-1}
+NEOVIM_LATEST_URL=$(cat ./neovim_latest.json | jq -r '.assets[] | select(.name == "nvim-linux64.tar.gz") | .browser_download_url')
 rm ./neovim_latest.json
-echo $NEOVIM_LATEST_URL
 curl -LJO $NEOVIM_LATEST_URL
+mkdir -pv ~/.local/apps/nvim
 mkdir -pv ~/.local/bin
-tar -xvf nvim-linux64.tar.gz -C ~/.local/ --strip-components 1
+tar -xvf nvim-linux64.tar.gz -C ~/.local/apps/nvim --strip-components 1
+rm nvim-linux64.tar.gz
+ln -s ~/.local/apps/nvim/bin/nvim ~/.local/bin/nvim
 
 # Install Python provider support
 python3 -m pip install --user --upgrade pynvim
