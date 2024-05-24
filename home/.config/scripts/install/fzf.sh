35884c7cc510e3d83d754904d8e8a6444907de46
shawngmc <7342147+shawngmc@users.noreply.github.com>
2024-05-24 19:53:06 -0400

    Create fzf.sh

---
+++ home/.config/scripts/install/fzf.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+source ../util/helper.sh
+
+if (( is_airgapped = 1 )); then
+  FZF_LATEST=$(curl -s "https://api.github.com/repos/junegunn/fzf/releases/latest")
+  FZF_TAG=$(echo $FZF_LATEST | jq -r '."tag_name"')
+  echo "Pulling FZF tag: ${FZF_TAG}"
+  
+  FZF_LATEST_URL=$(echo $FZF_LATEST | jq -r '.tarball_url')
+  curl -LJ $FZF_LATEST_URL -o ./fzf.tar.gz
+  mkdir ~/.fzf/
+  tar -xvf ./fzf.tar.gz --strip-components 1 -C ~/.fzf/
+  rm ./fzf.tar.gz
+  ~/.fzf/install --all
+else
+  echo "Installing older version from package repo...";
+  install_package fzf;
+fi
