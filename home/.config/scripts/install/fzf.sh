e9fcc670cdad6bff8ebfcc62692297ec49cbdf7e
shawngmc <7342147+shawngmc@users.noreply.github.com>
2024-05-24 19:54:39 -0400

    Update fzf.sh

--- home/.config/scripts/install/fzf.sh
+++ home/.config/scripts/install/fzf.sh
@@ -3,6 +3,10 @@
 source ../util/helper.sh
 
 if (( is_airgapped = 1 )); then
+  if (( does_cli_tool_exist jq == 1 )); then
+    echo "JQ is required to install FZF from repo..." >&2;
+    exit 1
+  fi
   FZF_LATEST=$(curl -s "https://api.github.com/repos/junegunn/fzf/releases/latest")
   FZF_TAG=$(echo $FZF_LATEST | jq -r '."tag_name"')
   echo "Pulling FZF tag: ${FZF_TAG}"
