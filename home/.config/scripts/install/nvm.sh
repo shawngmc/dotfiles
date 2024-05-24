47446f5cb922929694017dd73ea5cd5ff2af1a6f
shawngmc <7342147+shawngmc@users.noreply.github.com>
2024-05-24 19:51:37 -0400

    Create nvm.sh

---
+++ home/.config/scripts/install/nvm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Installing nvm..."
+PROFILE=/dev/null bash -c 'curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash'
+
+echo "Adding .bashrc.d file...";
+cat << 'EOF' > ~/.bashrc.d/nvm.sh
+#!/bin/bash
+
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+EOF
+
+echo "Install Latest Node LTS and npm..."
+nvm install --lts
+nvm install-latest-npm
