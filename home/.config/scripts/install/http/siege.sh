3debf60ca387e7faa7fd05c3552d1ee9d677e914
shawngmc <7342147+shawngmc@users.noreply.github.com>
2024-05-24 19:58:00 -0400

    Update and rename home/.config/scripts/install/http/siege.sh to home/.config/scripts/install/siege.sh

--- home/.config/scripts/install/http/siege.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source /etc/os-release
-
-if [ "${ID}" = "debian" ] | [ "${ID}" = "ubuntu" ]; then
-    sudo apt install -y siege
-elif [ "${ID}" = "redhat" ] | [ "${ID}" = "fedora" ] | [ "${ID}" = "rocky" ] | [ "${ID}" = "rhel" ]; then
-    sudo yum install -y siege
-fi
---
+++ home/.config/scripts/install/siege.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+source ../util/helper.sh
+
+echo "Installing siege from package repo...";
+install_package siege;
