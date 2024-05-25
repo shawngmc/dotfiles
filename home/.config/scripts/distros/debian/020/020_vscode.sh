10559c4b4a092469742c8e0c70d56759f12cd3b7
shawngmc <7342147+shawngmc@users.noreply.github.com>
2024-05-24 20:24:30 -0400

    Update 020_vscode.sh

--- home/.config/scripts/distros/debian/020/020_vscode.sh
+++ home/.config/scripts/distros/debian/020/020_vscode.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
 
-wget -O /tmp/vscode.deb 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64'
+source ../util/helper.sh
 
-sudo dpkg -i /tmp/vscode.deb
+echo "Installing vscode..."
+if (( is_airgapped = 1 )); then
+  DETECTED_PACKAGE_MANAGER=detect_package_manager;
+  case "${DETECTED_PACKAGE_MANAGER}" in
+    apt)
+      FILETYPE=deb
+      ;;
 
-rm /tmp/vscode.deb
+    dnf)
+      FILETYPE=rpm
+      ;;
+
+    *)
+      echo "Unclear how package manager ${DETECTED_PACKAGE_MANAGER} works, but I don't handle it yet..." >&2;
+      exit 1;
+      ;;
+  esac
+  FILENAME="/tmp/vscode.${FILETYPE}"
+  URL="https://code.visualstudio.com/sha/download?build=stable&os=linux-${FILETYPE}-x64"
+  wget -O ${FILENAME} "\'${URL}\'"
+  install_os_package_manual ${FILENAME}
+  rm ${FILENAME}
+else
+  echo "VSCode isn't in package repos...";
+  exit 1;
+fi
