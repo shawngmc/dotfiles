a2581526fcfd12c326881a8c42ab496a155421f6
shawngmc <7342147+shawngmc@users.noreply.github.com>
2024-05-25 00:46:48 -0400

    Update fastfetch.sh

--- home/.config/scripts/install/fastfetch.sh
+++ home/.config/scripts/install/fastfetch.sh
@@ -8,13 +8,13 @@ install_package fastfetch;
 echo "Creating config...";
 fastfetch --structure Title:Separator:OS:Host:Kernel:Uptime:Packages:Shell:Display:DE:WM:Terminal:TerminalFont:CPU:GPU:Memory:Swap:Disk:LocalIp:Battery:PowerAdapter:Locale:Break:Colors --gen-config
 
-echo "Adding .bashrc.d file...";
-cat << 'EOF' > ~/.bashrc.d/fastfetch.sh
-#!/bin/bash
+# echo "Adding .bashrc.d file...";
+# cat << 'EOF' > ~/.bashrc.d/fastfetch.sh
+# #!/bin/bash
 
-# Run fastfetch if present
-FASTFETCH_PATH=$(which fastfetch)
-if [ $? -eq 0 ]; then
-  fastfetch
-fi
-EOF
+# # Run fastfetch if present
+# FASTFETCH_PATH=$(which fastfetch)
+# if [ $? -eq 0 ]; then
+#   fastfetch
+# fi
+# EOF
