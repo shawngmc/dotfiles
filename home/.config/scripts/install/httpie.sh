27c0046fc2fcc6c2bf254597122f649c3031f134
shawngmc <7342147+shawngmc@users.noreply.github.com>
2024-05-24 20:04:19 -0400

    Update and rename home/.config/scripts/install/http/httpie.sh to home/.config/scripts/install/httpie.sh

---
+++ home/.config/scripts/install/httpie.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+source ../util/helper.sh
+
+if (( is_airgapped = 1 )); then
+  if (( does_cli_tool_exist jq == 1 )); then
+    echo "JQ is required to install httpie from repo..." >&2;
+    exit 1
+  fi
+  
+  # Have to save pull from GH to protect newlines
+  curl -s "https://api.github.com/repos/httpie/cli/releases/latest" -o ./httpie_latest.json
+  HTTPIE_TAG=$(cat ./httpie_latest.json | grep tag_name)
+  HTTPIE_TAG=${JQ_TAG:15:-2}
+  echo "Pulling HTTPIE tag: ${HTTPIE_TAG}"
+  
+  HTTPIE_LATEST_URL=$(cat ./httpie_latest.json | jq -r '.assets[] | select(.name == "http") | .browser_download_url')
+  rm ./httpie_latest.json
+  echo $HTTPIE_LATEST_URL
+  curl -LJO $HTTPIE_LATEST_URL
+  chmod +x ./http
+  mkdir -pv ~/.local/bin
+  mv http ~/.local/bin/http
+else
+  echo "Installing older version from package repo...";
+  install_package httpie;
+fi
