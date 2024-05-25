b959c7db42b09e350bee9b4a4cb352c37f6c0c0b
Shawn McNaughton <7342147+shawngmc@users.noreply.github.com>
2024-05-25 01:40:37 -0400

    fix jq

--- .config/scripts/install/jq.sh
+++ .config/scripts/install/jq.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-source ../util/helper.sh
+source "$(dirname $0)/../util/helper.sh"
 
-if (( does_cli_tool_exist "jq" = 1 )); then
+if (( $(does_cli_tool_exist "jq") == 1 )); then
     echo "JQ already installed; not updating because this may be OS bundled...";
 else
-  if (( is_airgapped = 1 )); then
+  if (( $(is_airgapped) == 1 )); then
     # Can't use JQ to get the JQ version :)
     # Have to save pull from GH to protect newlines
-    curl -s "https://api.github.com/repos/jqlang/jq/releases/latest" -o ./jq_latest.json 
+    curl -s "https://api.github.com/repos/jqlang/jq/releases/latest" -o ./jq_latest.json
     JQ_TAG=$(cat ./jq_latest.json | grep tag_name)
     JQ_TAG=${JQ_TAG:15:-2}
     echo "Pulling JQ tag: ${JQ_TAG}"
-    
+
     JQ_LATEST_URL=$(cat ./jq_latest.json | grep browser_download_url | grep jq-linux-amd64)
     JQ_LATEST_URL=${JQ_LATEST_URL:31:-1}
     rm ./jq_latest.json
