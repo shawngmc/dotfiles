d543ed6bdfdbbdac76e803181081ff2ea4427fdb
Shawn McNaughton <7342147+shawngmc@users.noreply.github.com>
2024-05-25 01:34:39 -0400

    fix nvm install

--- .config/scripts/install/nvm.sh
+++ .config/scripts/install/nvm.sh
@@ -11,6 +11,10 @@ export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || pr
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 EOF
 
+echo "Activating NVM in this subshell..."
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
 echo "Install Latest Node LTS and npm..."
 nvm install --lts
 nvm install-latest-npm
