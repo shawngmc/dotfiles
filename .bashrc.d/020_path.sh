c67d546a3ce22f9751f33c5d8f69f9accf073f08
Shawn McNaughton <7342147+shawngmc@users.noreply.github.com>
2024-05-26 00:30:26 -0400

    .bashrc.d ordering

---
+++ .bashrc.d/020_path.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# User specific environment
+if ! [[ "$PATH" =~ "$HOME/.local/bin:$HOME/bin:" ]]
+then
+    PATH="$HOME/.local/bin:$HOME/bin:$PATH"
+fi
+export PATH
---
+++ .bashrc.d/140_powerline.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Run conda activate if present, then enable powerline
+CONDA_PATH=$(which conda 2>/dev/null)
+if [ $? -eq 0 ]; then
+  conda activate base
+  POWERLINE_PATH=$(python3 -m pip show powerline-status | grep "Location:" | sed "s/Location: //")/powerline
+  if [ -f ${POWERLINE_PATH}/bindings/bash/powerline.sh ]; then
+    powerline-daemon -q
+    POWERLINE_BASH_CONTINUATION=1
+    POWERLINE_BASH_SELECT=1
+    PROMPT_COMMAND=""
+    source ${POWERLINE_PATH}/bindings/bash/powerline.sh
+  fi
+fi
