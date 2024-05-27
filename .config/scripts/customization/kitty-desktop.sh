ea5316eb0f749359c2de84e61d2f50aa9efc77b1
shawngmc <7342147+shawngmc@users.noreply.github.com>
2024-05-27 19:27:35 -0400

    Update kitty-desktop.sh

--- .config/scripts/customization/kitty-desktop.sh
+++ .config/scripts/customization/kitty-desktop.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Current issues:
+# - First launch isn't fullscreen
+# - If we get to two actual copies of kitty with that class, wmctrl and xdotool get confused
+
+
 KITTY_DESKTOP_CLASS="kitty-desktop-mode"
 KITTY_SOCKET="unix://tmp/kittydesktop"
 
@@ -11,13 +16,20 @@ KITTY_DESKTOP_RUNNING=$?
 
 # If kitty desktop mode isn't running, start it
 if ((KITTY_DESKTOP_RUNNING == 1)); then 
-    kitty -o allow_remote_control=yes --listen-on "${KITTY_SOCKET}" --class "${KITTY_DESKTOP_CLASS}"
-    wmctrl -x -r "${KITTY_DESKTOP_CLASS}" -b add,hidden
-    wmctrl -x -r "${KITTY_DESKTOP_CLASS}" -b remove,fullscreen
-    sleep 2
+  kitty -o allow_remote_control=yes --listen-on "${KITTY_SOCKET}" --class "${KITTY_DESKTOP_CLASS}"
+  xdotool search --class "${KITTY_DESKTOP_CLASS}" windowunmap
+  sleep 1
 fi
 
+# Get the current state
+xprop -id $(xdotool search --class "${KITTY_DESKTOP_CLASS}") | grep 'window state: Withdrawn'
+KITTY_HIDDEN=$?
+
 # Toggle things
-wmctrl -x -r "${KITTY_DESKTOP_CLASS}" -b toggle,hidden
-wmctrl -x -r "${KITTY_DESKTOP_CLASS}" -b toggle,fullscreen
-wmctrl -x -r "${KITTY_DESKTOP_CLASS}" -b add,skip_taskbar
+if ((KITTY_HIDDEN == true)); then 
+  xdotool search --class "${KITTY_DESKTOP_CLASS}" windowmap 
+  wmctrl -x -r "${KITTY_DESKTOP_CLASS}" -b add,fullscreen
+else
+  wmctrl -x -r "${KITTY_DESKTOP_CLASS}" -b remove,fullscreen
+  xdotool search --class "${KITTY_DESKTOP_CLASS}" windowunmap 
+fi
