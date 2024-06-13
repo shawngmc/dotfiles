5eefb400fd632f0750adf5bfccaa908b83396862
Shawn McNaughton <7342147+shawngmc@users.noreply.github.com>
2024-06-13 00:15:49 -0400

    fixing brew install

--- .config/scripts/install/brew.sh
+++ .config/scripts/install/brew.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+source "$(dirname $0)/../util/helper.sh"
+
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+DETECTED_PACKAGE_MANAGER=$(detect_package_manager);
+case "${DETECTED_PACKAGE_MANAGER}" in
+  apt)
+    sudo apt-get install build-essential -y
+    ;;
+
+  dnf)
+    sudo dnf install make automake gcc gcc-c++ kernel-devel
+    ;;
+
+  *)
+    echo "Unclear how package manager ${DETECTED_PACKAGE_MANAGER} works, but I don't handle it yet..." >&2;
+    exit 1;
+    ;;
+esac
+
 brew install gcc
