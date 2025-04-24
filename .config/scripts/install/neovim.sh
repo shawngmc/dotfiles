2b99573ad06e79b4664c37670a291c67d8027179
shawngmc <7342147+shawngmc@users.noreply.github.com>
2025-04-24 17:24:58 -0400

    Update neovim.sh

--- .config/scripts/install/neovim.sh
+++ .config/scripts/install/neovim.sh
@@ -21,3 +21,20 @@ python3 -m pip install --user --upgrade pynvim
 # Install vim plugins
 install_package vim-airline;
 install_package vim-fugitive;
+
+# Install ShellCheck, which is stupidly case-sensitive
+DETECTED_PACKAGE_MANAGER=$(detect_package_manager);
+  case "${DETECTED_PACKAGE_MANAGER}" in
+    apt)
+      sudo apt install -y shellcheck;
+      ;;
+
+    dnf)
+      sudo dnf install -y ShellCheck;
+      ;;
+
+    *)
+      echo "Unclear how package manager ${DETECTED_PACKAGE_MANAGER} works, but I don't handle it yet..." >&2;
+      exit 1;
+      ;;
+  esac
